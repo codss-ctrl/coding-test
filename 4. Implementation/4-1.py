64df621c8ad69bc6c801d601dc693d7b8463646b
teraglass <thecodss@gmail.com>
2021-06-16 17:02:33 +0900

    modify code

--- 4. Implementation/4-1.py
+++ 4. Implementation/4-1.py
@@ -4,8 +4,8 @@ x, y = 1, 1
 plans = input().split()
 
 # L,R,U,D에 따른 이동방향
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 move_types = ['L', 'R', 'U', 'D']
 
 for plan in plans:
@@ -13,8 +13,9 @@ for plan in plans:
         if plan == move_types[i]:
             nx = x + dx[i]
             ny = y + dy[i]
+            break  # 연산 했으면 break
     # 공간을 벗어나는 경우 무시
-    if nx <1 or ny < 1 or nx > n or ny > n:
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
     # 이동 수행
     x, y = nx, ny
