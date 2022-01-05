1ca34c9c057dfde43ad397e25a9005283966cd41
tera <thecodss@gmail.com>
2022-01-06 03:26:35 +0900

    이진 탐색 트리

---
+++ LeetCode/비선형 자료구조/트리/이진 탐색 트리/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,12 @@
+# 전위 순회 결과로 중위 순회 분할 정복
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # 전위 순회 결과는 중위 순회 분할 인덱스
+        index = inorder.index(preorder.pop(0))
+
+        # 중위 순회 결과 분할 정복
+        node = TreeNode(inorder[index])
+        node.left = self.buildTree(preorder, inorder[0:index])
+        node.right =self.buildTree(preorder, inorder[index+1:])
+
+        return node
---
+++ LeetCode/비선형 자료구조/트리/이진 탐색 트리/108. Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,13 @@
+# 이진 검색 결과로 트리 구성
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mid = len(nums) //2
+
+        # 분할 정복으로 이진 탐색 결과 트리 구성
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return node
