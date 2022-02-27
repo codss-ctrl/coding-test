4fceac6b8dcff72825b5e87c2dc1c48e41673083
teraglass <67316314+teraglass@users.noreply.github.com>
2022-02-27 17:34:21 +0900

    fix

--- LeetCode/비선형 자료구조/트리/104. Maximum Depth of Binary Tree.py
+++ LeetCode/비선형 자료구조/트리/104. Maximum Depth of Binary Tree.py
@@ -28,3 +28,14 @@ class Solution:
                     queue.append(cur_root.right)
         # BFS 반복 횟수 == 깊이
         return depth
+
+
+
+# DFS 풀이
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+
+        return dfs(root, 0)
